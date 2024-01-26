b5b8b98b95a56807f9729b4b62f93f4cf783d695
annieseinna <107967636+annieseinna@users.noreply.github.com>
2024-01-26 12:53:26 -0500

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -4,19 +4,15 @@ public void fizzbuzz() {
 
     if (i % 5 == 0 && i % 3 == 0) {
       System.out.println("FizzBuzz");
-      System.out.prinln();
     }
     else if (i % 3 == 0) {
       System.out.println("Fizz");
-      System.out.prinln();
     }
     else if (i % 5 == 0) {
       System.out.println("Buzz");
-      System.out.prinln();
     }
     else {
       System.out.println(i);
-      System.out.prinln();
     }
   }
 }
